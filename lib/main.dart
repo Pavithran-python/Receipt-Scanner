5a6169e8ba4c6fb6de31bf9b88e7ba9b85f4b3fd
pavi <pavithran1095@gmail.com>
2025-08-09 21:24:23 +0530

    Final changes

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            theme: buildAppTheme(),
+            theme: AppTheme().buildAppTheme(),
             home: DashboardScreen(),
           );
         },
@@ -45,75 +45,5 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  ThemeData buildAppTheme(){
-    return ThemeData(
-      primaryColor: AppColors.primary,
-      scaffoldBackgroundColor: AppColors.background,
-      appBarTheme: AppBarTheme(
-        backgroundColor: AppColors.primary,
-        foregroundColor: AppColors.background,
-        elevation: AppSizes.appBarElevation,
-        titleTextStyle: TextStyle(color: AppColors.appBarTextColor,fontSize: AppSizes.appBarTextSize,fontWeight: FontWeight.bold),
-      ),
-      textTheme: TextTheme(
-        displayLarge: TextStyle(color: AppColors.error,fontSize:  AppSizes.displayLargeTextSize,fontWeight: FontWeight.bold),
-        displaySmall: TextStyle(color: AppColors.buttonTextColor,fontSize: AppSizes.displaySmallTextSize,fontWeight: FontWeight.w600),
-        bodyLarge: TextStyle(color: AppColors.text,fontSize: AppSizes.bodyLargeTextSize,fontWeight: FontWeight.w600),
-        bodyMedium: TextStyle(color: AppColors.text,fontSize: AppSizes.bodyMediumTextSize,fontWeight: FontWeight.w400),
-        bodySmall: TextStyle(color: AppColors.text,fontSize: AppSizes.bodySmallTextSize,),
-      ),
-      primarySwatch: AppColors.primary, // or your custom swatch
-      colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          foregroundColor: AppColors.background, // Text color
-          backgroundColor: AppColors.primary,
-          fixedSize: Size.fromHeight(AppSizes.buttonHeight),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(AppSizes.radius),
-          ),
-          textStyle: TextStyle(fontSize: AppSizes.buttonTextSize, fontWeight: FontWeight.bold,color: AppColors.buttonTextColor),
-        ),
-      ),
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: OutlinedButton.styleFrom(
-          foregroundColor: AppColors.primary,
-          side: BorderSide(color: AppColors.primary,width: AppSizes.buttonBorderWidth),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(AppSizes.radius),
-          ),
-          fixedSize: Size.fromHeight(AppSizes.buttonHeight),
-          textStyle: TextStyle(fontSize: AppSizes.buttonTextSize,fontWeight: FontWeight.bold,),
-        ),
-      ),
-      floatingActionButtonTheme: FloatingActionButtonThemeData(
-        sizeConstraints: BoxConstraints.tightFor(width: AppSizes.floatingButtonSize, height: AppSizes.floatingButtonSize),
-        backgroundColor: AppColors.primary,
-        foregroundColor: AppColors.buttonTextColor,
-        focusElevation: AppSizes.appBarElevation,
-        iconSize: AppSizes.floatingActionButtonIconSize,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(AppSizes.floatingButtonSize),
-        ),
-      ),
-      inputDecorationTheme: InputDecorationTheme(
-        labelStyle: TextStyle(color: AppColors.textFieldHintTextColor,),
-        helperStyle: TextStyle(color: AppColors.textFieldHintTextColor),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: AppColors.borderFocusColor, width: AppSizes.textFieldBorderWidth),
-          borderRadius: BorderRadius.circular(AppSizes.textFieldBorderRadius),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: AppColors.borderColor),
-          borderRadius: BorderRadius.circular(AppSizes.textFieldBorderRadius),
-        ),
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: AppColors.borderColor),
-          borderRadius: BorderRadius.circular(AppSizes.textFieldBorderRadius),
-        ),
-        contentPadding: EdgeInsets.symmetric(horizontal: AppSizes.textFieldHorizontalPadding, vertical: AppSizes.textFieldVerticalPadding),
-      ),
-    );
-  }
 }
 
